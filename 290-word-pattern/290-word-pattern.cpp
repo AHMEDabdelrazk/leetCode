acef13ff550095ac16e159138691377ebf5046b5
AHMEDabdelrazk <77356094+AHMEDabdelrazk@users.noreply.github.com>
2022-08-08 14:57:25 +0200

    Time: 0 ms (100.00%), Space: 6.5 MB (55.13%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    
-    // So Important And Informative Problem
-    
-   bool wordPattern(string pattern, string s) 
- {
-	 map<char, string> m;
-	 int index = 0;
-	 string myString;
-	 for (int i = 0; i < pattern.size(); i++)
-	 {
-		 myString = "";
-		  while((index < s.size()) && (s[index] != ' '))
-			 {
-				 myString.push_back(s[index]);
-				
-				 index++;
-			 }
-			 index++;
-		 if (m.find(pattern[i]) == m.end()) 
-		 {
-			 for (auto it = m.begin(); it != m.end(); it++)
-				 if (it->second == myString) return false;
-			 m[pattern[i]] = myString;
-		 }
-		 else
-		 {
-			 if (m[pattern[i]] != myString) return false;
-		 }
-		 if (index > (s.size()+1)) return false;
-	 }
-	 if (index < s.size()) return false;
-	 return true;
- }
-			 
-			 
-		
+    bool wordPattern(string patt, string str) 
+    {
+        vector<int> patt_map(26,0);
+        unordered_map<string,int> str_map;
+        istringstream  is(str);
+        string s;
+        int n=patt.size(),i=0;
+        while(is>>s)
+        {
+            if(i==n||patt_map[patt[i]-'a']!=str_map[s])
+            {
+                return false;
+            }
+            else
+            {
+                patt_map[patt[i]-'a']=str_map[s]=i+1;
+            }
+            i++;
+        }
+        return i==n;
+        
+    }
 };
