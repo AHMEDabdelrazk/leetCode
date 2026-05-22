283d3f5c2451b47909b2d1abdd98f95319f148e8
Ahmed Abdelrazik Ramadan <77356094+AHMEDabdelrazk@users.noreply.github.com>
2026-05-22 18:21:45 +0300

    Time: 0 ms (100%), Space: 53.2 MB (86.78%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        // Left half is sorted
+        if (nums[l] <= nums[mid]) {
+            if (nums[l] <= target && target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        } 
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+};
