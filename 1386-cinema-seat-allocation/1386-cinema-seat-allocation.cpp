726a466a4203fca6a08197c625764da1a642ab0f
Ahmed Abdelrazik Ramadan <77356094+AHMEDabdelrazk@users.noreply.github.com>
2026-08-19 16:28:26 +0300

    Time: 2219 ms (5.07%), Space: 53.8 MB (93.41%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& rs) {
         int res = 0, j = 1, mx = 0;
+        // sorting to walking from 1 to n and 
         std::sort(rs.begin(), rs.end());
+        // loop only over the reservedSeats
         for(int i=0;i<rs.size();i++){
             bool b1 = 0, b2 = 0, b3 = 0;
+            // the rows that is empty
             if(rs[i][0] != j) {res+=2; j++; --i; continue;}
+            // rows with reserved seats
             while(i<rs.size()){
                 if(std::clamp(rs[i][1], 2, 5) == rs[i][1]) b1 = 1;
                 if(std::clamp(rs[i][1], 4, 7) == rs[i][1]) b2 = 1;
@@ -13,14 +17,16 @@ public:
                 ++i;
                 if(i<rs.size()&&rs[i][0] != j){--i;break;} 
             }
+            // choose one of the two pattern results 
             int res1 = 0, res2 = 0;
             if(!b1) res1++;
             if(!b2) res2++;
             if(!b3) res1++;
             res+=max(res1,res2);
-            cout<<res;
+            // j is from 0 - n
             j++;
         }
+        // the rows that is empty after
         for(int i=j;i<=n;i++) res+=2;
         return res;
     }
