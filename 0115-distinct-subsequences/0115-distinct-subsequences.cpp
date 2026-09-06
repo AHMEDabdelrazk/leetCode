087e76faa2779524a9d57bfabdb9fd698728385d
Ahmed Abdelrazik Ramadan <77356094+AHMEDabdelrazk@users.noreply.github.com>
2026-09-06 18:12:25 +0300

    Time: 12 ms (84.8%), Space: 17.4 MB (77.69%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int tn = t.size(), sn = s.size();
-        int cnt = 0;
-        unsigned long long int dp[2][tn+1];
-        for(int i = 0 ; i<=sn ; i++)
-        {
-            for(int j = 0; j <= tn ; j++)
-            {
-                if(j==0)
-                    dp[i%2][0] = 1;
-                else if(i==0)
-                    dp[0][j] = 0;
-                else
-                {
-                    if(s[i-1] == t[j-1])
-                        dp[i%2][j] = dp[(i-1)%2][j-1] + dp[(i-1)%2][j];
-                    else
-                        dp[i%2][j] = dp[(i-1)%2][j];
+        int sn = s.size();
+        int tn = t.size();
+        unsigned long long dp[sn + 1][tn + 1];
 
+        // Base cases
+        for (int i = 0; i <= sn; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int j = 1; j <= tn; j++) {
+            dp[0][j] = 0;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= sn; i++) {
+            for (int j = 1; j <= tn; j++) {
+
+                if (s[i - 1] == t[j - 1]) {
+                    // Two choices:
+                    // 1. Use s[i-1]
+                    // 2. Skip s[i-1]
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+                else {
+                    // Characters don't match.
+                    // We must skip s[i-1].
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        return dp[sn%2][tn];
+
+        return dp[sn][tn];
     }
 };
