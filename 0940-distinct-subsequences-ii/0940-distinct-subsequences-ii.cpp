35e9a657c59914848b9e8f26fe7e7a5a5a24035a
Ahmed Abdelrazik Ramadan <77356094+AHMEDabdelrazk@users.noreply.github.com>
2026-09-08 18:15:12 +0300

    Time: 23 ms (9.51%), Space: 22.1 MB (5.13%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int distinctSubseqII(string s) {   
+         vector<vector<int>> dp(s.length() + 1, vector<int>(26, 0));
+        int n = s.size();
+        for (int i = 1; i <= s.length(); i++) {
+            dp[i] = dp[i - 1];
+            int c = s[i - 1] - 'a';
+            int total = 0;
+            for (int j = 0; j < 26; j++) {
+                total += dp[i - 1][j];
+                total = total%MOD;
+          }
+            dp[i][c] = total%MOD + 1;
+        }
+        int answer = 0;
+        for (int j = 0; j < 26; j++) {
+            answer += dp[s.length()][j];
+            answer = answer % MOD;
+        }
+        return answer;
+    }
+};
+
+/*
+Processed	char	ending in a	ending in b	ending in c	Total
+""      	—	    0	        0	        0	        0
+"a"	        a	    1	        0	        0	        1
+"ab"	    b	    1	        2	        0	        3
+"abc"	    c	    1	        2	        4	        7
+
+*/
